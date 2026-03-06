afd956dbd2224ccfb8383470a7525b993bd27fb7
Eliézer Michel <eliezer_michel@outlook.com>
2026-03-06 12:12:31 -0300

    feat: Adiciona verificação se há usuário ou não

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -20,7 +20,7 @@ export function renderProfile(userData, userRepos, container) {
     userData.name
   }" class="profile-avatar">
       <div class="profile-info">
-        <h2>${userData.name}</h2>
+        <h2>${userData.name || "Nome não informado"}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
       </div>
     </div>
